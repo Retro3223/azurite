d8e21d0210869ea3005c8d6ca98ceba7ecf1902e
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2017-09-04 18:08:31 -0700

    utilities for motor simulations

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='frc3223-azurite',
+    version='0.0.1',
+    packages=[
+        'frc3223_azurite',
+    ],
+    description='miscellaneous utilities for jupyter notebooks',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+)
+
